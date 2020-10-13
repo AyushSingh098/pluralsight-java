dc23d575590bea63e571df6d5cadec97fefc8854
Ayush Singh <ayush.singh@lowes.com>
2020-10-13 21:07:43 +0530

    Interactive mode using string

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -1,5 +1,7 @@
 package com.pluralsight.calcengine;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
@@ -21,11 +23,30 @@ public class Main {
     {
         handleCommandLine(args);
     }
-    else
+    else if(args.length ==1 && args[0].equals("interactive"))
     {
+        executeInteractively();
+    }
+    else {
         System.out.println("Error");
     }
+    }
 
+    static void executeInteractively(){
+        System.out.println("Enter and operation and two number");
+        Scanner scanner = new Scanner(System.in);
+        String userInput= scanner.nextLine();
+        String[] parts= userInput.split(" ");
+        performOperation(parts);
+    }
+
+    private static void performOperation(String[] parts) {
+        char opCode = opCodeFromString(parts[0]);
+        double leftVal = valueFromWord(parts[1]);
+        double rightVal = valueFromWord(parts[2]);
+        double result=0.0d;
+        result = execute(opCode,leftVal,rightVal);
+        System.out.println(result);
     }
 
     private static void handleCommandLine(String[] args) {
@@ -58,5 +79,25 @@ public class Main {
                 break;
         }
         return result;
+    }
+
+    static char opCodeFromString(String operationName){
+        char opCode = operationName.charAt(0);
+        return opCode;
+    }
+
+    static double valueFromWord(String word){
+        String[] numberWords = {"zero","one", "two", "three", "four",
+                                "five", "six", "seven", "eight", "nine"};
+        double value =0.0d;
+        for(int index=0;index <numberWords.length;index++){
+            if(word.equals(numberWords[index]))
+            {
+                value = index;
+                break;
+            }
+        }
+
+        return value;
     }
 }
