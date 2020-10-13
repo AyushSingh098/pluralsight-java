829a1fcb823bed144c2806ec92c83a29e9cba44a
Ayush Singh <ayush.singh@lowes.com>
2020-10-13 14:02:39 +0530

    change to switch statements

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -4,25 +4,28 @@ public class Main {
 
     public static void main(String[] args){
         double value1=100.0d;
-        double value2=2.0d;
+        double value2=0.0d;
         double result=0.0d;
         char opCode='d';
 
-        if(opCode =='a')
-            result=value1+value2;
-        else if(opCode=='s')
-            result=value1-value2;
-        else if(opCode=='d'){
-            if(value2 != 0)
-                result=value1/value2;
-            else
-                System.out.println("Dividing by 0");
-        }
-            else if(opCode=='m')
-            result=value1*value2;
-        else{
-            System.out.println("Invalid opCode= "+ opCode);
-            result=0;
+
+        switch(opCode){
+            case 'a':
+                result=value1+value2;
+                break;
+            case 's':
+                result=value1-value2;
+                break;
+            case 'm':
+                result=value1*value2;
+                break;
+            case 'd':
+                result = value2 !=0 ? value1/value2 : 0.0d;
+                break;
+            default:
+                System.out.println("Invalid OpCode");
+                result=0;
+                break;
         }
 
 
