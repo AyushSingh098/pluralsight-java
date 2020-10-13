b7f364e36951fe5388fa8c8e617128c034e1618c
Ayush Singh <ayush.singh@lowes.com>
2020-10-13 20:28:31 +0530

    command line support

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -3,20 +3,38 @@ package com.pluralsight.calcengine;
 public class Main {
 
     public static void main(String[] args){
-    double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-    double[] rightVals = {0.0d, 25.0d, 225.0d, 11.0d};
-    char[] opCodes = {'d', 'a', 's', 'm'};
-    double[] results = new double[opCodes.length];
+    if(args.length ==0) {
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {0.0d, 25.0d, 225.0d, 11.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
 
-    for(int index=0;index < opCodes.length;index++)
+        for (int index = 0; index < opCodes.length; index++) {
+            results[index] = execute(opCodes[index], leftVals[index], rightVals[index]);
+        }
+
+        for (double result : results) {
+            System.out.println(result);
+        }
+    }
+    else if(args.length ==3)
     {
-        results[index] = execute(opCodes[index], leftVals[index], rightVals[index]);
+        handleCommandLine(args);
+    }
+    else
+    {
+        System.out.println("Error");
     }
 
-    for(double result: results){
+    }
+
+    private static void handleCommandLine(String[] args) {
+        char opCode = args[0].charAt(0);
+        double leftVal = Double.parseDouble(args[1]);
+        double rightVal = Double.parseDouble(args[2]);
+        double result;
+        result  =execute(opCode,leftVal,rightVal);
         System.out.println(result);
-    }
-
     }
 
     static double execute(char opCode, double leftVal, double rightVal){
